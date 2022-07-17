6e7b865462bc12640a6920986171207d07759060
rmisu <roger889@umn.edu>
2022-07-17 16:11:55 -0500

    environment issue?

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './develop/public/index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${PORT}`);
 });
 
