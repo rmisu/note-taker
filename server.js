32770f58a4ba3cf91f3fd7394e59625ad9f88042
rmisu <roger889@umn.edu>
2022-07-17 16:00:37 -0500

    heroku please.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { notes } = require('./develop/db/db.json')
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
