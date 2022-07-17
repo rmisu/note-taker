40b8ee8119068aa680d4c445fa0854b852bb978c
rmisu <roger889@umn.edu>
2022-07-17 16:14:05 -0500

    why why why

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './develop/public/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, function() {
     console.log(`Server listening on port ${PORT}`);
 });
 
